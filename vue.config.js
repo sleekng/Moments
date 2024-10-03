f603492a4d1f45cff462c98ef7c0a0864e02618d
sleekng <153427570+sleekng@users.noreply.github.com>
2024-10-04 00:57:04 +0100

    10/4

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/https://github.com/sleekng/Moments.git/' // Replace with your repo name
+      ? '/Moments/' // Replace with your repo name
       : '/'
   }
   
